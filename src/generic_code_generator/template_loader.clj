1f044a7e81007575ae1c7ff62ffedbc5256d8f36
evinasgu <enyert.watson@gmail.com>
2019-02-26 18:10:57 -0500

    Added selmer code to the implementation of load-template

--- src/generic_code_generator/template_loader.clj
+++ src/generic_code_generator/template_loader.clj
@@ -1,1 +1,11 @@
-(ns generic-code-generator.template-loader)
+(ns generic-code-generator.template-loader
+  :require [clojure.tools.logging :as log])
+
+(use 'selmer.parser)
+
+(defn load-template
+  [template-content template-url]
+  (log/info (str "Loading the template: " template-url))
+  (render-file template-url template-content))
+  
+  
