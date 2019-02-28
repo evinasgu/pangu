2d432854b4bac1851a1a4e13937d4f9ba7096175
evinasgu <enyert.watson@gmail.com>
2019-02-28 02:30:57 -0500

    Added a request validator namespace

--- src/generic_code_generator/template_loader.clj
+++ src/generic_code_generator/template_loader.clj
@@ -6,6 +6,13 @@
 (defn load-template
   [template-content template-url]
   (log/info (str "Loading the template: " template-url))
+  (cond
+    (empty? template-content) (throw
+                               (ex-info "template-content cannot be empty or nil"
+                                        {:causes #{:empty-template-content}
+                                         :actual-value {:value template-content}}))
+    (empty? template-url) (throw
+                           (ex-info "template-url cannot be empty or nil"
+                                    {:causes #{:empty-template-url}
+                                     :actual-value {:value template-url}}))
   (render-file template-url template-content))
-  
-  
