36879ceba331717f3b244567ace93aba98617c7f
Enyert <enyert.watson@gmail.com>
2020-01-09 04:17:59 -0500

    Lite changes to retake the project

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :ring {:handler generic-code-generator.http-handler/handler, :open-browser false}
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [liberator "0.15.1"]
                  [compojure "1.6.0"]
                  [ring/ring-core "1.6.3"]
