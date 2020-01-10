40bfdb8e15fb75c74b0be122fa1dda2eb3fae4c9
Enyert <enyert.watson@gmail.com>
2020-01-10 00:42:54 -0500

    Added basic type constraint for map input

--- src/generic_code_generator/template_loader.clj
+++ src/generic_code_generator/template_loader.clj
@@ -7,7 +7,6 @@
 (defn load-template
   "This function takes a file located in template-url and make the substitution for the data presented in template-content argument"
   [template-content template-url]
-  (log/info (str "Loading the template: " template-url))
   (cond
     (empty? template-content) (throw
                                (ex-info "template-content cannot be empty or nil"
@@ -17,4 +16,8 @@
                            (ex-info "template-url cannot be empty or nil"
                                     {:causes #{:empty-template-url}
                                      :actual-value {:value template-url}}))
+    (not (map? template-content)) (throw
+                                    (ex-info "template-content has to be a map"
+                                             {:causes #{:incompatible-type-template-content}
+                                              :actual-value {:value template-content}}))
     :else (render-file template-url template-content)))             
--- test/generic_code_generator/template_loader_test.clj
+++ test/generic_code_generator/template_loader_test.clj
@@ -3,14 +3,19 @@
             [clojure.java.io :as io]
             [generic-code-generator.template-loader :refer :all]))
 
+(def home-folder-location (System/getProperty "user.home"))
+
 (def expected-template-loaded "bla")
 
 (def valid-test-input-template "template TODO")
 
-(deftest load-template-test)
-  ;(testing "load-template function load succesfully a valid template"
-  ;  (is (= (load-template valid-test-input-template) expected-template-loaded;))))
-
-(deftest validate-template-test) ;TODO
-
-(deftest write-template-test) ;TODO
+(deftest load-template-test
+  (testing "load-template returns exception when template-content argument is empty"
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"template-content cannot be empty or nil"
+                          (load-template nil home-folder-location))))
+  (testing "load-template returns exception when template-url is empty or nil"
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"template-url cannot be empty or nil"
+                          (load-template {:key "value"} nil))))
+  (testing "load-template returns exception when template-content is not a map"
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"template-content has to be a map"
+                          (load-template home-folder-location "hello")))))
